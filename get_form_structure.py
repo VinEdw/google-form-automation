751de8f586befd2e1e619fb371d15d1af99bf21b
Vincent Edwards <mister.man489@gmail.com>
2022-06-30 10:07:50 -0700

    refactored consecutive function calls into single function

--- get_form_structure.py
+++ get_form_structure.py
@@ -92,11 +92,18 @@ def parse_form_json(form_json):
 
     return form_info
 
+def form_info_from_url(url: str):
+    """
+    Get the structural information about the Form at the input url.
+    """
+    form_json = get_form_json(url)
+    form_info = parse_form_json(form_json)
+    return form_info
+
 if __name__ == "__main__":
     url = "https://docs.google.com/forms/d/e/1FAIpQLScykTPZxLRtTXHAxxVN8l8RVPxzcfokD_HMkc5Hbio4sq3p_g/viewform"
     # url = input("Please input the url for the Google Form.\n")
-    form_json = get_form_json(url)
-    form_info = parse_form_json(form_json)
+    form_info = form_info_from_url(url)
     print(form_info)
     for question in form_info["questions"]:
         print(question)
